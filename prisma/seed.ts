00239cf348c6e10849f2420e0529f04efe121072
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2022-04-13 19:40:30 +0200

    feat: created prisma models, migrations and seeds

---
+++ prisma/seed.ts
@@ -0,0 +1,70 @@
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import { artistsData } from './artistsData'
+
+const prisma = new PrismaClient()
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async artist => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map(s => ({
+              name: s.name,
+              url: s.url,
+              duration: s.duration
+            }))
+          }
+        }
+      })
+    })
+  );
+
+
+  const salt = bcrypt.genSaltSync()
+  const user = await prisma.user.upsert({
+    where: { email: 'user@test.com' },
+    update: {},
+    create: {
+      password: bcrypt.hashSync('password', salt),
+      email: 'user@test.com',
+      // playlists: 
+    }
+  })
+
+  const songs = await prisma.song.findMany({})
+
+  await Promise.all(
+    new Array(10)
+      .fill(1)
+      .map(async (_, i) => {
+        return prisma.playlist.create({
+          data: {
+            name: `Playlist #${i + 1}`,
+            user: {
+              connect: { id: user.id }
+            },
+            songs: {
+              connect: songs.map(song => ({
+                id: song.id
+              }))
+            }
+          }
+        })
+      })
+  )
+}
+
+
+
+run()
+  .catch(e => {
+    console.log(e)
+    process.exit(1)
+  })
+  .finally(() => {
+    prisma.$disconnect()
+  })
